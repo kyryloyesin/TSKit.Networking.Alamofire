969696d999a93a9ec7fa0393d09005820ce9248d
Arkadii Hlushchevskyi <adya.glu@gmail.com>
2020-10-17 18:48:14 +0300

    Implemented retrying through Alamofire.

--- TSKit.Networking.Alamofire/Sources/AlamofireNetworkService.swift
+++ TSKit.Networking.Alamofire/Sources/AlamofireNetworkService.swift
@@ -192,6 +192,7 @@ private extension AlamofireNetworkService {
             to: url,
             method: method,
             headers: headers,
+            interceptor: constructRetrier(with: call.request),
             requestModifier: { [weak call, weak self] in
                 if let timeout = call?.request.timeoutInterval ?? self?.configuration.timeoutInterval {
                     $0.timeoutInterval = timeout
@@ -225,6 +226,7 @@ private extension AlamofireNetworkService {
                                            parameters: call.request.parameters,
                                            encoding: encoding,
                                            headers: headers,
+                                           interceptor: constructRetrier(with: call.request),
                                            requestModifier: { [weak call, weak self] in
                                             if let timeout = call?.request.timeoutInterval ?? self?.configuration.timeoutInterval {
                                                 $0.timeoutInterval = timeout
@@ -242,6 +244,7 @@ private extension AlamofireNetworkService {
                                           parameters: call.request.parameters,
                                           encoding: encoding,
                                           headers: headers,
+                                          interceptor: constructRetrier(with: call.request),
                                           requestModifier: { [weak call, weak self] in
                                             if let timeout = call?.request.timeoutInterval ?? self?.configuration.timeoutInterval {
                                                 $0.timeoutInterval = timeout
@@ -281,6 +284,18 @@ private extension AlamofireNetworkService {
 
     func constructHeaders(withRequest request: AnyRequestable) -> HTTPHeaders {
         .init((defaultHeaders ?? [:]) + (request.headers ?? [:]))
+    }
+    
+    func constructRetrier(with request: AnyRequestable) -> RetryPolicy? {
+        let requestRetryAttempts = request.retryAttempts?.nonZero
+        guard requestRetryAttempts != nil || configuration.retriableMethods.contains(request.method) else { return nil }
+        guard let retries = requestRetryAttempts ?? configuration.retryAttempts?.nonZero else { return nil }
+        
+        let failures = request.retriableFailures ?? configuration.retriableFailures ?? RetryPolicy.defaultRetryableURLErrorCodes
+        return RetryPolicy(retryLimit: retries,
+                           retryableHTTPMethods: [HTTPMethod(request.method)],
+                           retryableHTTPStatusCodes: Set(100..<600).subtracting(request.statusCodes),
+                           retryableURLErrorCodes: failures)
     }
 }
 
@@ -720,12 +735,14 @@ private extension Alamofire.HTTPMethod {
 
     init(_ method: RequestMethod) {
         switch method {
-        case .get: self = .get
-        case .post: self = .post
-        case .patch: self = .patch
-        case .delete: self = .delete
-        case .put: self = .put
-        case .head: self = .head
+            case .get: self = .get
+            case .post: self = .post
+            case .patch: self = .patch
+            case .delete: self = .delete
+            case .put: self = .put
+            case .head: self = .head
+            case .trace: self = .trace
+            case .options: self = .options
         }
     }
 }
