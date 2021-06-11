8db1675c9d140ec2e19ca4d24eae9fd47cbff2c1
Arkadii Hlushchevskyi <adya.glu@gmail.com>
2021-06-11 21:21:25 +0300

    Updated copyright.

--- Sources/TSKit.Networking.Alamofire/AlamofireRequestCallBuilder.swift
+++ Sources/TSKit.Networking.Alamofire/AlamofireRequestCallBuilder.swift
@@ -1,6 +1,6 @@
 // - Since: 01/20/2018
 // - Author: Arkadii Hlushchevskyi
-// - Copyright: © 2020. Arkadii Hlushchevskyi.
+// - Copyright: © 2021. Arkadii Hlushchevskyi.
 // - Seealso: https://github.com/adya/TSKit.Networking.Alamofire/blob/master/LICENSE.md
 
 import Foundation
